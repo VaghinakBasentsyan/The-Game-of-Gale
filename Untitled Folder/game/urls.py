0f6f997ecf6ec984f9a5f52e875edf48ccfa1dda
VaghinakSTDev <vaghinak@stdevmail.com>
2019-03-01 15:08:34 +0400

    second change

--- Untitled Folder/game/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('(?P<factory_id>[0-9a-f-]+)', views.index, name='index'),
-
-]
